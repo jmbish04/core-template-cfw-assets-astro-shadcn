9a315c469f292654af0b80c65adbc747441a431c
jmbish04 <jmbish04@gmail.com>
2026-03-09 02:13:17 -0700

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,23 @@
 // @ts-check
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 
-const site = process.env.VERCEL
-  ? process.env.VERCEL_ENV === "production"
-    ? "https://astro-shadcn-ui-template.vercel.app"
-    : `https://${process.env.VERCEL_URL}`
-  : (process.env.SITE ?? "http://localhost:4321");
+const site = process.env.SITE ?? "http://localhost:4321";
 const base = process.env.BASE || "/";
 
 // https://astro.build/config
 export default defineConfig({
   site,
   base,
+  output: "server",
+  adapter: cloudflare({
+    imageService: "cloudflare",
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
